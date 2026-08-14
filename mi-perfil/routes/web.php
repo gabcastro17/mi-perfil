6f440b30b3a1d37af0b86b349e24fbc99fe4db99
Gabriela Castro <gabrielacastro@192.168.40.118>
2026-08-13 22:31:47 -0500

    rutas creadas

--- mi-perfil/routes/web.php
+++ mi-perfil/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/perfil', function () {
+    return view('perfil');
+});
+Route::get('/intereses', function () {
+    return view('intereses');
+});
+Route::get('/habilidades', function () {
+    return view('habilidades');
+});
+Route::get('/metas', function () {
+    return view('metas');
+});
